7212c08c8e6ee29e221cba0b432ac5c2e8363c6c
Greg Degruy <gregory.degruy@gmail.com>
2016-05-24 22:59:13 -0400

    renaming project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blinky")]
+[assembly: AssemblyTitle("Eureka")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blinky")]
+[assembly: AssemblyProduct("Eureka")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
