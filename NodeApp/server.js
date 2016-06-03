27c47d92fc71e8c4b255f821e550dd37adf932fc
Nate <rose.nathanielm@gmail.com>
2016-06-02 18:09:54 -0700

    Added working Node App
    Functioning node app with modules and organized RP3 code from Node App

---
+++ NodeApp/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+var redis = require('redis')
+var osc = require('osc')
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser')
+
+var lastPointTime = Date.now();
+var now;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded())
+app.use(express.static(__dirname + '/public'));
+
+var udpPort = new osc.UDPPort({
+    localAddress: "127.0.0.1",
+    localPort: 5000
+});
+
+udpPort.open();
+
+io.on('connection', function (socket) {
+    console.log("socket.io connection");
+  	socket.emit('news', { hello: 'world' });  
+/*
+	setInterval(function(){
+		var testData = {
+			address: "/muse/eeg",
+			args: [100, 200, 300, 400]
+		}
+		socket.emit('news', testData); 
+	}, 1000);
+*/
+  	// Listen for incoming OSC bundles.
+	udpPort.on("message", function (oscData) {
+		now = Date.now()
+		if((now-lastPointTime <= 1000) || (lastPointTime-now <= 1000)) {
+			lastPointTime = now
+			socket.emit('news', oscData); 
+		}
+	});
+
+});
+
+var port = Number(process.env.PORT || 3000);
+server.listen(port, function() {
+  console.log("Listening on " + port);
+});
